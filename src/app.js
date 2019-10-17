216d0df222ae11725d435abe868608d5e1c122e2
FabioJSilva <fabiopt6@gmail.com>
2019-10-16 21:13:16 -0300

    Configuraçao do sequelize e estrutura das pastas

--- src/app.js
+++ src/app.js
@@ -6,15 +6,15 @@ class App {
     this.server = express();
     this.middleware();
     this.routes();
-  };
+  }
 
   middleware() {
     this.server.use(express.json());
-  };
+  }
 
   routes() {
     this.server.use(routes);
-  };
-};
+  }
+}
 
 export default new App().server;
