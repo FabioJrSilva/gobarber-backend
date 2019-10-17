bfc91774ba5b0bdd1b91a670a93b61dd0c9caf34
FabioJSilva <fabiopt6@gmail.com>
2019-10-16 21:55:28 -0300

    Criaçao da model de users

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Word' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Fabio Junior',
+    email: 'fabio2@exemplo.com',
+    password_hash: '12312313',
+  });
+  res.json(user);
+});
 
 export default routes;
