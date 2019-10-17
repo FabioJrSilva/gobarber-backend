0e8a6836da854f5dd7cb04bffc09d53e22358312
FabioJSilva <fabiopt6@gmail.com>
2019-10-17 17:45:25 -0300

    Cadastro de usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Fabio Junior',
-    email: 'fabio2@exemplo.com',
-    password_hash: '12312313',
-  });
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
