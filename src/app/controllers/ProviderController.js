cdb67ec6b5e1e6c268c204d684c31a269460f5ff
Fabio <fabio.junior@uotz.com.br>
2019-10-25 14:46:23 -0300

    Listagem de providers

---
+++ src/app/controllers/ProviderController.js
@@ -0,0 +1,26 @@
+import User from '../models/User';
+import File from '../models/File';
+
+class ProviderController {
+  async index(req, res) {
+    const providers = await User.findAll({
+      where: { provider: true },
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['name', 'path', 'url']
+        }
+      ]
+    });
+
+    if (!providers.length) {
+      res.json({ message: 'There are no providers.' });
+    }
+
+    return res.json(providers);
+  }
+}
+
+module.exports = new ProviderController();
