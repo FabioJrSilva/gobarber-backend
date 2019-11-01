98d9c7d119aa9525f61e1ba92508e8f68cac1e7a
Fabio <fabio.junior@uotz.com.br>
2019-11-01 12:52:53 -0300

    Configuração do Nodemailer e envio de email quando ouver um cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'c297f35a5c4e8b',
+    pass: '092ffd64a44fc9'
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+  }
+};
